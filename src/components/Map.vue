13e2559d1725a7bb59b9cad6c4a06645863a4199
gal-gordon <65529433+gal-gordon@users.noreply.github.com>
2020-09-13 18:08:39 +0300

    added user location on start

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,16 @@
 <template>
-<div id="map" style="width:100vw; height:85vh">
+<div>
 
+<div id="map" style="width:100vw; height:85vh">
   <MglMap
     :accessToken="mapboxAccessToken"
     :mapStyle.sync="mapStyle"
     :center="coordinates"
+    
   >
     <MglMarker :coordinates="coordinates" color="blue" />
   </MglMap>
+</div>
 </div>
 </template>
 
@@ -25,8 +28,25 @@ export default {
       mapStyle: "mapbox://styles/galgordon/ckf15jvyz2or61at0ww55hapq",
       coordinates: [-111.549668, 39.014]
     };
-  }
-};
+  },
+  methods:{
+    getLocation: function(){
+    var location = []
+   navigator.geolocation.getCurrentPosition(
+    position => {
+    location = [position.coords.longitude,position.coords.latitude]
+    this.coordinates = location;
+  },
+     error => {
+       return error.message;
+     },
+)   
+}
+    },
+    beforeMount(){
+    this.getLocation()
+    }
+  };
 </script>
 
 <style scoped>
