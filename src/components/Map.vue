ee805c55c645ee6893bab0158b8742263e8ff7a8
gal-gordon <65529433+gal-gordon@users.noreply.github.com>
2020-09-13 17:29:10 +0300

    added pin location

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,33 +1,37 @@
 <template>
-<div id='map' style='width: 100vw; height: 85vh'>
-  <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
+<div id="map" style="width:100vw; height:85vh">
+
+  <MglMap
+    :accessToken="mapboxAccessToken"
+    :mapStyle.sync="mapStyle"
+    :center="coordinates"
+  >
+    <MglMarker :coordinates="coordinates" color="blue" />
+  </MglMap>
 </div>
 </template>
 
 <script>
-import Mapbox from "mapbox-gl";
-import { MglMap } from "vue-mapbox";
+import { MglMap, MglMarker } from "vue-mapbox";
 
 export default {
   components: {
-    MglMap
+    MglMap,
+    MglMarker
   },
   data() {
     return {
-      accessToken: "pk.eyJ1IjoiZ2FsZ29yZG9uIiwiYSI6ImNrZjE1Y2JuYjEwbjQyenBobGJ2ZGg2dDgifQ.AN0iqcgNOu9E-u83IhE8nQ", // your access token. Needed if you using Mapbox maps
-      mapStyle: "mapbox://styles/galgordon/ckf15jvyz2or61at0ww55hapq" // your map style
+      mapboxAccessToken:"pk.eyJ1IjoiZ2FsZ29yZG9uIiwiYSI6ImNrZjE2b280MzB2azIyeG8wbTdxazBvaXUifQ.UrMcasMni4SJeCf7KGYQrA",
+      mapStyle: "mapbox://styles/galgordon/ckf15jvyz2or61at0ww55hapq",
+      coordinates: [-111.549668, 39.014]
     };
-  },
-
-  created() {
-    // We need to set mapbox-gl library here in order to use it in template
-    this.mapbox = Mapbox;
   }
 };
 </script>
+
 <style scoped>
 #map {
-    position: fixed;
-    bottom: 0;
+  position: fixed;
+  bottom: 0;
 }
 </style>
