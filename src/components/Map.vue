f4aa1b9ceec9cdb619a7b9720a68c56d713b5f59
gal-gordon <65529433+gal-gordon@users.noreply.github.com>
2020-09-13 16:59:41 +0300

    added correct map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,42 +1,33 @@
 <template>
-  <div>
-    <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true"
-             data-projection="EPSG:4326" style="height: 400px">
-      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
-
-      <vl-geoloc @update:position="geolocPosition = $event">
-        <template slot-scope="geoloc">
-          <vl-feature v-if="geoloc.position" id="position-feature">
-            <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
-            <vl-style-box>
-              <vl-style-icon src="_media/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
-            </vl-style-box>
-          </vl-feature>
-        </template>
-      </vl-geoloc>
-
-      <vl-layer-tile id="osm">
-        <vl-source-osm></vl-source-osm>
-      </vl-layer-tile>
-    </vl-map>
-    <div style="padding: 20px">
-      Zoom: {{ zoom }}<br>
-      Center: {{ center }}<br>
-      Rotation: {{ rotation }}<br>
-      My geolocation: {{ geolocPosition }}
-    </div>
-  </div>
+<div id='map' style='width: 100vw; height: 85vh'>
+  <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
+</div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return { 
-        zoom: 5,
-        center: [0, 0],
-        rotation: 0,
-        geolocPosition: undefined,
-      }
-    }
+import Mapbox from "mapbox-gl";
+import { MglMap } from "vue-mapbox";
+
+export default {
+  components: {
+    MglMap
+  },
+  data() {
+    return {
+      accessToken: "pk.eyJ1IjoiZ2FsZ29yZG9uIiwiYSI6ImNrZjE1Y2JuYjEwbjQyenBobGJ2ZGg2dDgifQ.AN0iqcgNOu9E-u83IhE8nQ", // your access token. Needed if you using Mapbox maps
+      mapStyle: "mapbox://styles/galgordon/ckf15jvyz2or61at0ww55hapq" // your map style
+    };
+  },
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
   }
-  </script>
+};
+</script>
+<style scoped>
+#map {
+    position: fixed;
+    bottom: 0;
+}
+</style>
